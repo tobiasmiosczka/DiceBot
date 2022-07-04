c25f156fb1db0431a885a946e748942501930e20
TobiasMiosczka <6351397+tobiasmiosczka@users.noreply.github.com>
2022-07-04 02:40:12 +0200

    -refactoring

--- src/main/java/com/github/tobiasmiosczka/dicebot/commands/RollCommand.java
+++ src/main/java/com/github/tobiasmiosczka/dicebot/commands/RollCommand.java
@@ -1,6 +1,5 @@
 package com.github.tobiasmiosczka.dicebot.commands;
 
-import com.github.tobiasmiosczka.dicebot.discord.JdaUtil;
 import com.github.tobiasmiosczka.dicebot.discord.command.documentation.Option;
 import com.github.tobiasmiosczka.dicebot.discord.command.CommandFunction;
 import com.github.tobiasmiosczka.dicebot.discord.command.documentation.Command;
@@ -13,14 +12,17 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import static com.github.tobiasmiosczka.dicebot.discord.JdaUtil.quoted;
+import static com.github.tobiasmiosczka.dicebot.discord.JdaUtil.underlined;
+import static com.github.tobiasmiosczka.dicebot.parsing.DiceNotationParser.parseDiceNotation;
+import static com.github.tobiasmiosczka.dicebot.parsing.DiceNotationParser.parseRollNotation;
+
 @Command(
         command = "r",
         description = "Rolls some dices.",
         arguments = {
-                @Option(
-                        name = "roll",
-                        type = OptionType.STRING,
-                        description = "Definition of the Roll.")})
+                @Option(name = "roll", type = OptionType.STRING, description = "Definition of the Roll.")
+        })
 public class RollCommand implements CommandFunction {
 
     @Override
@@ -28,16 +30,17 @@ public class RollCommand implements CommandFunction {
         if (event.getOption("roll") == null)
             return event.reply("Roll what?");
         String arg = event.getOption("roll").getAsString();
-        String rolls = DiceNotationParser.parseDiceNotation(arg);
-        String formula = DiceNotationParser.parseRollNotation(rolls);
+        String rolls = parseDiceNotation(arg);
+        String formula = parseRollNotation(rolls);
+        String user = event.getUser().getAsMention();
         try {
             String result = DiceNotationParser.calculate(formula, 10, TimeUnit.SECONDS);
-            return event.reply(event.getUser().getAsMention() + ": " + JdaUtil.quoted(arg) + "\n" + rolls + " = " + JdaUtil.underlined(result));
+            return event.reply(user + ": " + quoted(arg) + "\n" + rolls + " = " + underlined(result));
         } catch (TimeoutException e) {
-            return event.reply(event.getUser().getAsMention() + ": " + JdaUtil.quoted(arg) + "\nSorry, this is too complicated for me.");
+            return event.reply(user + ": " + quoted(arg) + "\nSorry, this is too complicated for me.");
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
-            return event.reply(event.getUser().getAsMention() + ": " + JdaUtil.quoted(arg) + "\nSorry, something went wrong.:thinking:");
+            return event.reply(user + ": " + quoted(arg) + "\nSorry, something went wrong.:thinking:");
         }
     }
 }
